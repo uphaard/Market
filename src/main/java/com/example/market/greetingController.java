148645ba74ede063cc30b99ebc9d4674dedbb3d2
Uphaar Dubey <uphaardubey@NOVI-C02FFBZEMD6M.local>
2021-07-05 18:39:29 +0530

    version1.0

---
+++ src/main/java/com/example/market/greetingController.java
@@ -0,0 +1,17 @@
+package com.example.market;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class greetingController {
+    public greetingController(){
+
+    }
+    @Value("${greeting}")
+    private String greetingMessage;
+
+    public void greeting() {
+        System.out.println("!!! "+ this.greetingMessage);
+    }
+}
